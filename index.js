8fcba8f464de05db7438fe0751d17c23796f4fd0
Nicolas <menosiinicolas@gmail.com>
2025-03-19 17:52:13 +0000

    aula1903

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => { 
+    response.send('Ola mundo!');}
+);
+app.get('/mensagem', (request, response) => {
+    response.send('Boa tarde');
+}
+)
+app.listen(port, () => {
+    console.log("Aplicação em execução na porta" + port);
+}
+);
